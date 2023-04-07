bc01bfff3aae172c6e1d1a1971307f90426ceeed
Tania Boterman <taboterm@c2s4d10.42wolfsburg.de>
2023-04-07 16:27:07 +0200

    linked list addfront, addback, lstlast, lstnew, added

---
+++ libft_new/ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 15:31:09 by taboterm          #+#    #+#             */
+/*   Updated: 2023/04/07 16:25:32 by taboterm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+/* 
+* add the node to the "new" at the end of the list
+* @param lst: pointer to a pointer of the stuct called t_list
+* @param new: pointer to t_list struct represents the new node added to
+* the end of the list 
+* return: none
+*/ 
+
---
+++ libft_new/ft_lstadd_front copy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front copy.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/06 19:45:38 by taboterm          #+#    #+#             */
+/*   Updated: 2023/04/06 19:51:33 by taboterm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+lst: the address of a pointer to the first link of ta list
+new: the address of a pointer to the node to be added to 
+the list
+return: none
+Adds the node to the "new" at the beginning of the list
+*/
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	
+}
